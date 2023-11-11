e9b93ad9a623ad5b898bc9bac4f4cb13ece8aa44
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-10 21:32:24 -0500

    Update ModeloUsuario.java

--- EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/modelo/ModeloUsuario.java
+++ EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/modelo/ModeloUsuario.java
@@ -1,4 +1,17 @@
 package com.example.envioemailapi_springboot.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class ModeloUsuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
 }
