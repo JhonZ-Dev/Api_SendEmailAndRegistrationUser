3910f2eaf31fbaea88ba1e4dff16d4076322d7cd
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-10 21:33:44 -0500

    Update UsuarioRepositorio.java

--- EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/repositorio/UsuarioRepositorio.java
+++ EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/repositorio/UsuarioRepositorio.java
@@ -1,4 +1,9 @@
 package com.example.envioemailapi_springboot.repositorio;
 
-public interface UsuarioRepositorio {
+import com.example.envioemailapi_springboot.modelo.ModeloUsuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepositorio extends JpaRepository<ModeloUsuario, Long> {
+
+    ModeloUsuario findByEmail(String email);
 }
