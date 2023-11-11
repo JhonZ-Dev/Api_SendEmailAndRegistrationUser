c3f078353b62e4f1dfb0ed65eb1fc7805451f0c9
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-10 21:37:14 -0500

    Update ServicioUsuario.java

--- EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/servicio/ServicioUsuario.java
+++ EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/servicio/ServicioUsuario.java
@@ -1,5 +1,6 @@
 package com.example.envioemailapi_springboot.servicio;
 
+import com.example.envioemailapi_springboot.modelo.ModeloUsuario;
 import com.example.envioemailapi_springboot.repositorio.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,5 +14,16 @@ public class ServicioUsuario {
     public ServicioUsuario(UsuarioRepositorio usuarioRepositorio, JavaMailSender mailSender) {
         this.usuarioRepositorio = usuarioRepositorio;
         this.mailSender = mailSender;
+    }
+    public void registrarUsuario(ModeloUsuario user){
+        //verificar si el usuario ya existe
+        //if(usuarioRepositorio.findByEmail(user.getEmail()) ==null){
+        //guardamos en la BD
+        usuarioRepositorio.save(user);
+
+        // Envía el correo de confirmación
+        sendRegistrationEmail(user.getEmail());
+
+        //}
     }
 }
