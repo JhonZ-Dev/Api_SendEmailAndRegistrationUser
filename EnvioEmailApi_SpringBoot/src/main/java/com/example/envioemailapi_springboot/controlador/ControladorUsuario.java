a8fedc4b30f3213f0adf78bee6945438b0e4a1a2
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-10 21:38:50 -0500

    Update ControladorUsuario.java

--- EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/controlador/ControladorUsuario.java
+++ EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/controlador/ControladorUsuario.java
@@ -1,5 +1,7 @@
 package com.example.envioemailapi_springboot.controlador;
 
+import com.example.envioemailapi_springboot.servicio.ServicioUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,5 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 public class ControladorUsuario {
-    
+
+    private final ServicioUsuario usuarioServicio;
+
+
+
+    @Autowired
+    public ControladorUsuario(ServicioUsuario usuarioServicio) {
+        this.usuarioServicio = usuarioServicio;
+    }
 }
