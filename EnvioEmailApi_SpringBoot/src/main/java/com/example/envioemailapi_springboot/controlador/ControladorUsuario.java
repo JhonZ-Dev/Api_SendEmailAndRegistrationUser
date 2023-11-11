11ab94f380d289b094e23a823812ed6643610345
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-10 21:38:15 -0500

    Create ControladorUsuario.java

---
+++ EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/controlador/ControladorUsuario.java
@@ -0,0 +1,8 @@
+package com.example.envioemailapi_springboot.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControladorUsuario {
+}
