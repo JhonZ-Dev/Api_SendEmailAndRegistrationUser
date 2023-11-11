3f5ca8c5c6174cb863fdb7a9f18500367157f2b8
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-10 21:39:07 -0500

    Update ControladorUsuario.java

--- EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/controlador/ControladorUsuario.java
+++ EnvioEmailApi_SpringBoot/src/main/java/com/example/envioemailapi_springboot/controlador/ControladorUsuario.java
@@ -1,8 +1,12 @@
 package com.example.envioemailapi_springboot.controlador;
 
+import com.example.envioemailapi_springboot.modelo.ModeloUsuario;
 import com.example.envioemailapi_springboot.servicio.ServicioUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +21,16 @@ public class ControladorUsuario {
     @Autowired
     public ControladorUsuario(ServicioUsuario usuarioServicio) {
         this.usuarioServicio = usuarioServicio;
+    }
+    @PostMapping("/register")
+    public String registerUser(@RequestBody ModeloUsuario user) {
+        usuarioServicio.registrarUsuario(user);
+        return "Mensaje Enviado";
+    }
+
+    @PostMapping("/registrar/usuario")
+    public ResponseEntity<String> registrarUsuario(@RequestBody ModeloUsuario usuario){
+        usuarioServicio.registrarUsuario(usuario);
+        return ResponseEntity.ok("Usuario Registrado");
     }
 }
